88ffc4f4f440f71a3c87d923790f9005f86c97ff
HolaSoyLuis <luis95velasquez@outlook.com>
2019-01-11 15:19:57 -0800

    Sound and snake points added

--- game.py
+++ game.py
@@ -4,39 +4,66 @@ import pygame.event
 
 pygame.init()
 
-screen_width = 720
-screen_height = 640
+square_size = 16
+square_x = 45
+square_y = 40
+screen_width = square_x * square_size
+screen_height = square_y * square_size
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Snake')
+
+# background
+pygame.mixer.music.load('sound/dk.mp3')
+pygame.mixer.music.play()
+grow = pygame.mixer.Sound('sound/grow.wav')
+
+def draw_background():
+    background_img = pygame.image.load('img/white.png')
+    for i in range(0, square_y):
+        for j in range(0, square_x):
+            screen.blit(background_img, (j * square_size, i * square_size))
+
+# Game resolutions per square size
+# 16*30 = 480
+# 16*40 = 640
+# 16*45 = 720
+# end background
+
+# snake and point
+class snake_point:
+    x, y = 0, 0
+    img = pygame.image.load('img/red.png')
+
+    def __init__(self, x_, y_):
+        self.x = x_
+        self.y = y_
+
+    def draw_image(self):
+        screen.blit(self.img, (self.x * square_size, self.y * square_size))
 
 
 class snake:
     image = pygame.image.load('img/red.png')
-    snake_body = 1
+    snake_body = []
     snake_is_in_live = True
-    snake_x, snake_y = 0, 0
     score = 0
 
+    # start point
+    snake_x, snake_y = 0, 0
+    aux = snake_point(0, 0)
+
     def __init__(self):
-        self.snake_x = random.randint(0, screen_width / 16)
-        self.snake_y = random.randint(0, screen_height / 16)
+        self.snake_x = random.randint(0, screen_width / square_size)
+        self.snake_y = random.randint(0, screen_height / square_size)
+        self.snake_body.append(snake_point(self.snake_x, self.snake_y))
 
-    def draw_snake(self):
-        screen.blit(snake.image, (self.snake_x * 16, self.snake_y * 16))
+    def add_point(self, x_, y_):
+        self.snake_body.append(snake_point(x_, y_))
 
-
-def draw_background(screen):
-    background_img = pygame.image.load('img/white.png')
-    for i in range(0, 40):
-        for j in range(0, 45):
-            screen.blit(background_img, (j * 16, i * 16))
-
-
-'''    
-16*30 = 480
-16*40 = 640
-16*45 = 720
-'''
+    def draw_points(self):
+        for i in range(len(self.snake_body)):
+            # print(len(self.snake_body))
+            self.snake_body[i].draw_image()
 
 
 class point:
@@ -44,16 +71,16 @@ class point:
     img = pygame.image.load('img/green.png')
 
     def __init__(self):
-        self.x = random.randint(0, screen_width / 16)
-        self.y = random.randint(0, screen_height / 16)
+        self.x = random.randint(0, screen_width / square_size)
+        self.y = random.randint(0, screen_height / square_size)
 
     def get_random(self):
-        self.x = random.randint(0, screen_width / 16)
-        self.y = random.randint(0, screen_height / 16)
+        self.x = random.randint(0, screen_width / square_size)
+        self.y = random.randint(0, screen_height / square_size)
 
     def draw_image(self):
-        screen.blit(self.img, (self.x * 16, self.y * 16))
-
+        screen.blit(self.img, (self.x * square_size, self.y * square_size))
+# end snake and point
 
 snk = snake()
 pnt = point()
@@ -65,22 +92,52 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                snk.snake_y += 1
+                # snk.snake_y += 1
+                snk.snake_body[0].y += 1
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                snk.snake_y -= 1
+                # snk.snake_y -= 1
+                snk.snake_body[0].y -= 1
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                snk.snake_x -= 1
+                # snk.snake_x -= 1
+                snk.snake_body[0].x -= 1
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                snk.snake_x += 1
+                # snk.snake_x += 1
+                snk.snake_body[0].x += 1
 
-    draw_background(screen)
+    draw_background()
     pnt.draw_image()
-    snk.draw_snake()
-    if snk.snake_x == pnt.x and snk.snake_y == pnt.y:
+    snk.draw_points()
+    # print(len(snk.snake_body))
+    '''
+    for i in range(len(snk.snake_body)):
+        if snk.snake_body[i].x == pnt.x and snk.snake_body[i].y == pnt.y:
+            snk.score += 1
+            grow.play()
+            pnt.get_random()
+            print('Score is: ', snk.score)
+    '''
+    # if snk.snake_x == pnt.x and snk.snake_y == pnt.y:
+    if snk.snake_body[0].x == pnt.x and snk.snake_body[0].y == pnt.y:
         snk.score += 1
+        grow.play()
+        
+        snk.add_point(snk.snake_body[0].x, snk.snake_body[0].y)
         pnt.get_random()
         print('Score is: ', snk.score)
+        print(len(snk.snake_body))
+    
     pygame.display.update()
+
+
+'''
+la serpiente tiene un punto inicial que se mueve
+pero no debe ser asi, la serpiente debe de tener una dimension
+inicial la cual marque su punto inicial y actualize tanto a este como al resto
+para la actualizacion debo de crear un punto auxiliar el cual contenga la posicion
+del punto inicial y se vaya cambiando en valor de posicion y cada uno de los n puntos
+de la serpiente, asi el punto auxiliar sera actualizado en cada iteracion dando asi
+la posicion al siguiente punto
+'''
